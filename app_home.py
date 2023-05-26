cac82200681fc4ca1fd9ae3391eeac6e458db5f9
jjni <130967351+kje0058@users.noreply.github.com>
2023-05-26 11:47:26 +0900

    기획2_수정사항수정완료

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def run_app_home():
-    st.subheader('안녕하세요~!')
+    st.subheader('안녕하세요~!^^')
 
     st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다.')
     st.text('데이터 분석도 가능하고, 고객 정보를 넣으면 구매 차량 가격도 예측해줍니다.')
